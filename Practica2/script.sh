44c84610ba33e1dd47ce3ad3d635625262e38686
JJavier98 <jota.jota.98@hotmail.com>
2018-04-10 12:15:14 +0200

    martes 10/4/2018 12.14

--- Practica2/script.sh
+++ Practica2/script.sh
@@ -6,7 +6,7 @@ NUM2=$NUM1+1
 DAT=$NUM*$NUM1*$NUM2
 
 while [ $NUM -ge 1 ]; do
-    ./a.out $DAT >> max_datos_dyv.txt
+    ./a.out $DAT >> datos_dyv.txt
 	let NUM=$NUM-1
 	let NUM1=$NUM+1
 	let NUM2=$NUM1+1
